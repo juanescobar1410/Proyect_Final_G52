0a2bd765756eb24e1253fc446ae5cb2a45b532ed
Fanieruao <163151143+Fanieruao@users.noreply.github.com>
2025-11-13 18:40:27 -0500

    Codigo Boss(en desarrollo)

---
+++ PryectoFinal_G52/Assets/Game/Scripts/HitBoos.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class HitBoos : MonoBehaviour
+{
+    public int damage;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            //coll.GetComponent<PlayerHealth>().TakeDamage(damage);
+        }
+    }
+}
