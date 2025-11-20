843f2d22aa7c10207b2761950b5cb2e18fd31b91
juanescobar1410 <escobarsanchezjuanandres@gmail.com>
2025-11-19 19:04:53 -0500

    Cambios minimos

--- PryectoFinal_G52/Assets/Game/Scripts/Scene3/ControllerScene3.cs
+++ PryectoFinal_G52/Assets/Game/Scripts/Scene3/ControllerScene3.cs
@@ -1,20 +1,4 @@
 using UnityEngine;
-<<<<<<< HEAD
-using UnityEngine.SceneManagement;
-
-public class DoorTrigger : MonoBehaviour
-{
-    [Header("Nombre de la escena a cargar")]
-    public string sceneName;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            SceneManager.LoadScene(sceneName);
-        }
-    }
-=======
 using TMPro;
 
 public class SceneController : MonoBehaviour
